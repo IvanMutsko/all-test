97baeea2042f19d47683db7f296a4c4d2ff5f787
Ivan Mutsko <imucko1996@gmail.com>
2023-03-31 20:11:25 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,44 @@
-const movieToSave = 157336;
-
-// function add new movie in localStorage
-function addToWatched(movieId, string) {
+// function of adding new movie to localStorage
+function addMovieToLocalStorage(keyLibrary, movieId) {
   const movieList = [];
 
   try {
-    // перевірка чи пусте
-    const isEmpty = localStorage.getItem(`${string}`);
-
-    // якщо масив пустий то записуємо перший ключ та об'єкт з першим ІД
+    // checking for values
+    const isEmpty = localStorage.getItem(`${keyLibrary}`);
+    // if the array is empty - write down the first key and the array
     if (!isEmpty) {
       movieList.push(movieId);
-      localStorage.setItem(`${string}`, JSON.stringify(movieList));
+      localStorage.setItem(`${keyLibrary}`, JSON.stringify(movieList));
       return;
     }
-
-    // якщо є якісь дані то перевіряємо чи є в об'єкті таке значення
+    // if there is any data - we check whether the array contains such a value
     if (isEmpty.includes(movieId)) {
-      console.log('такий фільм є');
       return;
     }
-
-    // якщо фільма нема, треба його додати
-    // localStorage.getItem(watchedList)
-    const arr = JSON.parse(localStorage.getItem(`${string}`));
-
-    arr.push(movieId);
-
-    localStorage.setItem(`${string}`, JSON.stringify(arr));
+    // if there is no movie, we add movie
+    const arrMovieId = JSON.parse(localStorage.getItem(`${keyLibrary}`));
+    arrMovieId.push(movieId);
+    localStorage.setItem(`${keyLibrary}`, JSON.stringify(arrMovieId));
   } catch (error) {
     console.log(error);
   }
 }
 
-addToWatched(5555, 'watched');
-addToWatched(5555, 'qweqweqwe');
-addToWatched(7676767, 'qweqweqwe');
+// function of deleting the movie from the localStorage
+function removeMovieFromLocalStorage(keyLibrary, movieId) {
+  try {
+    const arrMovieId = JSON.parse(localStorage.getItem(`${keyLibrary}`));
+    // looking for the index of the element to delete
+    const index = arrMovieId.indexOf(movieId);
+    // remove an element from the array
+    if (index !== -1) {
+      arrMovieId.splice(index, 1);
+      // updated array in LocalStorage
+      localStorage.setItem(`${keyLibrary}`, JSON.stringify(arrMovieId));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-// try/catch
+export { addMovieToLocalStorage, removeMovieFromLocalStorage };
