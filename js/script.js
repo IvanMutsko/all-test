8b20ceb284bb7675ab76d40ebefd4164abe67fc7
Ivan Mutsko <imucko1996@gmail.com>
2023-04-02 14:12:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -138,22 +138,3 @@ console.log(isStringRotated('javascript', 'scriptjava')); // -> true
 console.log(isStringRotated('javascript', 'iptjavascr')); // -> true
 console.log(isStringRotated('javascript', 'java')); // -> false
 
-
-
-
-
-
-
-
-
-
-import { getDatabase, ref, set } from 'firebase/database';
-
-function writeUserData(userId, name, email, imageUrl) {
-  const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
-    profile_picture: imageUrl,
-  });
-}
