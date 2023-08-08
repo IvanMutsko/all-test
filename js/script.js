a6072022011b949dd084aebced8a362f724d3489
Ivan Mutsko <imucko1996@gmail.com>
2023-08-08 18:37:45 +0300

    repeat

--- js/script.js
+++ js/script.js
@@ -1,140 +1,533 @@
-// ============================================================
-// Уникальность всех символов в строке
+// =================================================
 
-// Напишите функцию, которая определяет уникальны ли все символы в строке. Регистр должен учитываться: `‘a’` и `‘A’` разные символы.
+// const getData = async url => {
+//   const res = await fetch(url);
+//   const json = await res.json();
 
-function isUnique(string) {
-  // 1
-  // for (let i = 0; i < string.length; i += 1) {
-  //   if (string.lastIndexOf(string[i]) !== i) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
+//   return json;
+// };
 
-  // 2
-  return [...new Set(string)].join('') === string;
-}
+// const url = 'https://jsonplaceholder.typicode.com/todos/';
 
-// console.log(isUnique('abcdef')); // -> true
-// console.log(isUnique('1234567')); // -> true
-// console.log(isUnique('abcABC')); // -> true
-// console.log(isUnique('abcadef')); // -> false
+// try {
+//   const data = await getData(url);
+//   console.log(data);
+// } catch (error) {
+//   console.error(error.message);
+// }
 
-// ============================================================
-// Плоский массив
+// =================================================
 
-// Напишите функцию, принимающая массив с вложенными массивами и распакуйте в результирующий плоский массов. В результате должны получить новый одномерный массив.
+// const asyncFn = async () => {
+//   return 'Success!';
+// };
 
-function flatten(array) {
-  const res = [];
+// const asyncFn = async () => {
+//   throw new Error('Bla bla bla....')
+// };
 
-  for (let i = 0; i < array.length; i += 1) {
-    if (!Array.isArray(array[i])) {
-      res.push(array[i]);
-    } else {
-      const flat = flatten(array[i]);
-      console.log(flat);
-      for (let j = 0; j < flat.length; j += 1) {
-        res.push(flat[j]);
-      }
-    }
-  }
-  return res;
-}
+// asyncFn()
+//   .then(value => console.log(value))
+//   .catch(error => console.error(error));
 
-// console.log(flatten([[1], [[2, 3]], [[[[[4]]]]]])); // -> [1, 2, 3, 4]
+// const timerPromise = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(), 2000);
+//   });
 
-// ============================================================
+// const asyncFn = async () => {
+//   console.log('Timer start');
+//   const startTime = performance.now();
+//   await timerPromise();
+//   const endTime = performance.now();
+//   console.log('Timer ended', endTime - startTime);
+// };
 
-// Удаление всех повторяющихся значений в строке
+// asyncFn();
 
-// Напишите функцию, которая принимает строку и возвращает новую, в которой все дублирующиеся символы будут удалены.
+// =================================================
 
-function removeDupes(str) {
-  // 1
-  // let unic = '';
-  // for (let i = 0; i < str.length; i += 1) {
-  //   if (str.indexOf(str[i]) === i) {
-  //     unic += str[i];
-  //   }
-  // }
-  // return unic;
+// const myPromise = new Promise((resolve, reject) => {});
 
-  // 2
-  return [...new Set(str)].join('');
-}
+// myPromise.then(value => {}).catch(error => {});
 
-// console.log(removeDupes('abcd')); // -> 'abcd'
-// console.log(removeDupes('aabbccdd')); // -> 'abcd'
-// console.log(removeDupes('abcddbca')); // -> 'abcd'
-// console.log(removeDupes('abababcdcdcd')); // -> 'abcd'
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+//   .catch(error => console.error(error));
 
-// ============================================================
+// const getData = url =>
+//   new Promise((resolve, reject) => {
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(json => resolve(json))
+//       .catch(error => reject(error));
+//   });
 
-// Какая строка встречается чаще всего
+// getData('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error.message));
 
-// Напишите функцию, которая принимает массив строк и возвращает самую частовстречающуюся строку в этом массиве. Если таких строк несколько, то нужно вернуть первую, идя слева на право.
+// =================================================
 
-function highestFrequency(array) {
-  // 1
-  //   const resObj = {};
-  //   let maxValue = 0;
-  //   let result = '';
+// class NumbersArray extends Array {
+//   sum() {
+//     return this.reduce((el, acc) => (acc += el), 0);
+//   }
+// }
 
-  //   for (let i = 0; i < array.length; i += 1) {
-  //     resObj[array[i]] = 0;
-  //   }
-  //   for (let i = 0; i < array.length; i += 1) {
-  //     resObj[array[i]] += 1;
-  //   }
+// const myArr = new NumbersArray(2, 5, 7);
 
-  //   const arrKeys = Object.keys(resObj);
-  //   const arrValues = Object.values(resObj);
+// console.log(myArr);
+// myArr.sum();
 
-  //   for (let i = 0; i < arrValues.length; i += 1) {
-  //     if (maxValue < arrValues[i]) {
-  //       maxValue = arrValues[i];
-  //       result = arrKeys[i];
-  //     }
-  //   }
+// console.log(myArr.sum());
 
-  //   return result;
+// const myName = 'Ivan';
+// console.log(myName);
+// console.log(myName.toLocaleUpperCase());
 
-  // 2 (не моє рішення)
-  const resObj = new Map();
-  let maxValue = 0;
-  let result = '';
+// =================================================
 
-  for (let i = 0; i < array.length; i += 1) {
-    if (!resObj.has(array[i])) {
-      resObj.set(array[i], 0);
-    }
-    resObj.set(array[i], resObj.get(array[i]) + 1);
-    if (resObj.get(array[i]) > maxValue) {
-      maxValue = resObj.get(array[i]);
-      result = array[i];
-    }
-  }
-  return result;
-}
+// class Comment {
+//   constructor(text) {
+//     (this.text = text), (this.votesQty = 0);
+//   }
 
-// console.log(highestFrequency(['a', 'b', 'c', 'c', 'c', 'd', 'e'])); // -> c
-// console.log(highestFrequency(['abc', 'def', 'abc', 'abc', 'def', 'abc'])); // -> abc
-// console.log(highestFrequency(['abc', 'def', 'abc', 'def'])); // -> abc
-// console.log(highestFrequency(['abc', 'abc', 'def', 'def', 'def', 'ghi', 'ghi', 'ghi', 'ghi'])); // -> ghi
+//   static mergeComments(first, second) {
+//     return `${first} ${second}`;
+//   }
 
-// ============================================================
+//   upvote() {
+//     this.votesQty += 1;
+//   }
+//   downvote() {
+//     this.votesQty -= 1;
+//   }
+// }
 
-// Повернута ли строка ?
+// const test = Comment.mergeComments('First comment', 'Second comment');
 
-// Напишите функцию, которая принимает 2 строки. Верните `true` если строки являются перевернутым вариантом друг друга (см. пример). Иначе верните `false`.
+// const myComment = new Comment('My new comment');
 
-function isStringRotated(source, test) {
-  // todo
-}
+// console.log(Comment);
+// console.log(myComment);
+// console.log(test);
 
-console.log(isStringRotated('javascript', 'scriptjava')); // -> true
-console.log(isStringRotated('javascript', 'iptjavascr')); // -> true
-console.log(isStringRotated('javascript', 'java')); // -> false
+// =================================================
 
+// class Comment {
+//   constructor(text) {
+//     (this.text = text), (this.votesQty = 0);
+//   }
+
+//   upvote() {
+//     this.votesQty += 1;
+//   }
+//   downvote() {
+//     this.votesQty -= 1;
+//   }
+// }
+
+// console.log(Comment);
+
+// const firstComment = new Comment('My first comment');
+// firstComment.upvote();
+// firstComment.upvote();
+// firstComment.upvote();
+
+// firstComment.downvote();
+
+// console.log(firstComment);
+
+// const isCommentEx = firstComment instanceof Comment;
+
+// console.log(isCommentEx); //перевірка приналежності до класу
+
+// console.log(firstComment.text);
+// console.log(firstComment.votesQty);
+
+// console.log(firstComment.hasOwnProperty('text'));
+// console.log(firstComment.hasOwnProperty('votesQty'));
+// console.log(firstComment.hasOwnProperty('upvote'));
+// console.log(firstComment.hasOwnProperty('downvote'));
+// console.log(firstComment.hasOwnProperty('hasOwnProperty'));
+
+// const secondComment = new Comment('My second comment');
+// const thirdComment = new Comment('My third comment');
+
+// =================================================
+
+// const myArr = [true, 124, 'qwe', undefined, null, [], {}];
+// console.log(myArr);
+
+// const typesArr = [];
+
+// for (let i = 0; i < myArr.length; i++) {
+//   console.log(typeof myArr[i]);
+//   typesArr.push(typeof myArr[i]);
+// }
+
+// console.log(typesArr);
+
+// myArr.forEach((el, idx) => {
+//   console.log(`"${el}" have index: ${idx}`);
+// });
+
+// let i = 0;
+
+// while (i <= 3) {
+//   console.log(i);
+//   i++;
+// }
+
+// let y = 5;
+
+// do {
+//   console.log(y);
+//   y += 5;
+// } while (y < 25);
+
+// for (const key in myArr) {
+//   console.log(myArr[key]);
+// }
+
+// const person = {
+//   name: 'Ivan',
+//   age: 27,
+//   city: 'Lviv',
+// };
+
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+
+// Object.keys(person).forEach(el => console.log(person[el]));
+
+// const text = 'qwased';
+
+// for (let el of text) {
+//   console.log(el);
+// }
+
+// =================================================
+
+// const sumFn = (a, b) => {
+//   return a + b;
+// };
+
+// const value1 = 3;
+// const value2 = 0;
+
+// value1 && value2 ? console.log(sumFn(value1, value2)) : console.log('One is args 0');
+
+// =================================================
+
+// const key = 4;
+
+// switch (key) {
+//   case 1:
+//     console.log(`Key is 1`);
+//     break;
+//   case 2:
+//     console.log(`Key is 2`);
+//     break;
+//   case 3:
+//     console.log(`Key is 3`);
+//     break;
+
+//   default:
+//     console.log(`Key is not a 1/2/3`);
+//     break;
+// }
+
+// =================================================
+
+// let val = 10;
+
+// if (val === 5) {
+//   console.log((val = 5));
+// }
+// if (val === 10) {
+//   console.log((val = 10));
+// }
+// if (val === 20) {
+//   console.log((val = 20));
+// }
+
+// const person = {
+//   name: 'Ivan',
+//   age: 27,
+//   city: 'Lviv',
+// };
+
+// if (!person.name) {
+//   console.log(`Name is undefined`);
+// } else {
+//   console.log(`Person name is ${person.name}`);
+// }
+
+// const sumFn = (a, b) => {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     return 'One of arguments NaN';
+//   }
+//   return a + b;
+// };
+
+// console.log(sumFn(10, 5));
+// console.log(sumFn('asd', 5));
+
+// =================================================
+
+// const person = {
+//   name: 'Ivan',
+//   age: 27,
+//   city: 'Lviv',
+//   logFunc: value => 'Inner function...',
+// };
+
+// const { name, age, city, logFunc } = person;
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// console.log(logFunc());
+
+// const fruits = ['Apple', 'Banana', 'Mango'];
+
+// const [first, second, third] = fruits;
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// =================================================
+
+// const myArr = [1, 2, 3];
+
+// console.log(myArr);
+
+// myArr.push(5);
+// console.log(myArr);
+
+// const removedEl = myArr.pop();
+// console.log(removedEl);
+
+// myArr.unshift(true);
+
+// const removedEl2 = myArr.shift();
+// console.log(removedEl2);
+
+// myArr.forEach(el => console.log(el * 3));
+
+// console.log(myArr);
+
+// const newArr = myArr.map(el => el * 5);
+// console.log(newArr);
+
+// =================================================
+
+// const myArr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(myArr);
+
+// const myArr2 = new Array(1, 2, 3, 4, 5, 6, 7);
+// console.log(myArr2);
+
+// const myArr3 = myArr;
+
+// console.log(myArr === myArr2);
+// console.log(myArr === myArr3);
+// console.log(myArr.length);
+// console.log(myArr[4]);
+// console.log(myArr[10]);
+
+// myArr.length = 15;
+// console.log(myArr);
+// console.log(myArr[10]);
+
+// =================================================
+
+// const myFn = a => {
+//   console.log(a);
+// };
+
+// myFn(5 === 2);
+
+// =================================================
+
+// const FnWithError = () => {
+//   throw new Error('Some error in function...');
+// };
+
+// try {
+//   FnWithError();
+// } catch (error) {
+//   console.error(error.message);
+// }
+
+// console.log('Continue...');
+
+// =================================================
+
+// function myFn(a, b) {
+//   let c;
+//   a += 1;
+//   c = a + b;
+//   return c;
+// } // можна випадково переназначити
+
+// // myFn = 5
+// console.log(myFn);
+
+// const myFn2 = function (a, b) {
+//   let c;
+//   a += 1;
+//   c = a + b;
+//   return c;
+// };
+
+// const myFn3 = (a, b = 5) => {
+//   let c;
+//   a += 1;
+//   c = a + b;
+//   return c;
+// };
+
+// console.log(myFn3(2));
+// console.log(myFn3(2, 6));
+
+// const newPost = (post, addedAt = Date()) => ({
+//   ...post,
+//   addedAt,
+// });
+
+// const post = {
+//   title: 'Learn JS',
+//   text: 'Learning JS is amazing',
+// };
+
+// console.table(newPost(post));
+
+// setTimeout(function () {
+//   console.log('Test in timeout');
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log('Test in timeout');
+// }, 3000);
+
+// =================================================
+
+// console.log(4 && true && 0);
+// console.log(0 || false || null);
+// const text = 'Hello';
+// const text2 = 'World';
+// console.log(text + ' ' + 'World' + '!');
+// console.log(`${text} ${text2}! My name Ivan`);
+
+// =================================================
+
+// const a = 5;
+
+// function myFn() {
+//   function innerFn() {
+//     console.log(a);
+//   }
+//   innerFn();
+// }
+
+// myFn();
+
+// let q;
+// let w;
+
+// function fn2() {
+//   let w;
+//   q = 10;
+//   w = 4;
+
+//   console.log(w);
+// }
+
+// fn2();
+
+// console.log(q);
+// console.log(w);
+
+// =================================================
+
+// function fn(a, b) {
+//   return a + b;
+// }
+
+// const first = 3;
+// const second = 5;
+
+// console.log(fn(first, second));
+
+// console.dir(fn);
+
+// const person = {
+//   name: 'Key',
+//   age: 27,
+//   city: 'Lviv',
+// };
+
+// function increaseAge(personObj) {
+//   const updatedPerson = { ...personObj };
+//   updatedPerson.age += 1;
+
+//   return updatedPerson;
+// }
+
+// const newPerson = increaseAge(person);
+// console.log(newPerson);
+// console.log(person);
+
+// function printMyName() {
+//   console.log('Ivan');
+// }
+
+// setTimeout(printMyName, 2000);
+
+// =================================================
+
+// console.log(JSON.stringify({ name: 'John', age: 30, car: null }));
+
+// const age = 22;
+
+// const person = {
+//   name: 'Key',
+//   age,
+//   city: 'Lviv',
+//   logFunc: value => {
+//     console.log(value);
+//   },
+// };
+
+// console.log(person);
+
+// person.name = 'Ivan';
+// delete person.age;
+
+// const log = 'active';
+// person[log] = true;
+
+// person.skills = {};
+
+// person.skills.js = true;
+// person.skills.css = true;
+// person.skills.html = true;
+
+// person.logFunc('I`m in func');
+
+// console.table(person);
+
+// const b = { ...person }; //вкладений об'єкт мутується
+// const c = Object.assign({}, person); //вкладений об'єкт мутується
+// const d = JSON.parse(JSON.stringify(person)); //вкладдені обєкти не мутуються
+
+// b.age = 27;
+// console.log(b);
+// console.table(person);
+// console.log(c);
+// console.log(d);
